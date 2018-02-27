abb0745e6ed82113b3a5dbf33243336cc1dfeb91
Emma O'Neill <emmaruthoneill@gmail.com>
2018-02-27 15:03:53 -0800

    added more things for interactive

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ console.log(windowWidth);
 var margin = {
   top: 15,
   right: 15,
-  bottom: 120,
+  bottom: 130,
   left: 80
 };
 var height, width;
@@ -36,7 +36,7 @@ var updateInfo = function(season) {
   document.querySelector(".info").innerHTML = `<strong>${season}</strong>`;
 };
 
-var ages = [20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38];
+var ages = [20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41];
 var i = 0;
 
 var loop = null;
